56e24196ca8068bdea2d0186b5ec0bb0125b0e8c
premkumart <tprem1718@gmail.com>
2025-02-17 18:23:19 -0500

    img src

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = ({theme,setTheme}) => {
              <h1 className="text-primary font-extrabold text-sm sm:text-2xl">Where in the world?</h1>
         </NavLink>
         <div className="flex flex-row gap-2 items-center cursor-pointer" onClick={()=>setTheme((theme)=> theme === 'light' ? 'dark' : 'light')}>
-            <img  className="h-4 w-4 lg:h-5 lg:w-5 " src={`${theme=='light' ? "./assets/moon-svgrepo-com.svg" : "./assets/light-svgrepo-com.svg" }`} alt="moon icon" />
+            <img  className="h-4 w-4 lg:h-5 lg:w-5 " src={`${theme=='light' ? "../assets/moon-svgrepo-com.svg" : "../assets/light-svgrepo-com.svg" }`} alt="moon icon" />
             <p className="text-primary">{theme=="light" ? "Dark"  : "Light" }</p>
       
         </div>
