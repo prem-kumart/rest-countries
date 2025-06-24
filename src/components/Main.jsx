9e50c8f3240a2ebcf268304959b367cbd5aa4817
premkumart <tprem1718@gmail.com>
2025-06-24 18:28:53 -0400

    Updated Endpoint

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,119 +1,136 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' 
-import {faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
-import { useEffect, useState, useRef } from 'react'
-import Country from './Country.jsx'
-import Header from './Header.jsx'
-import CustomSelect from './CustomSelect.jsx'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState, useRef } from "react";
+import Country from "./Country.jsx";
+import Header from "./Header.jsx";
+import CustomSelect from "./CustomSelect.jsx";
 
-const regionFilters = ['Africa','America','Asia','Europe','Oceania']
+const regionFilters = ["Africa", "America", "Asia", "Europe", "Oceania"];
 
-const Main = ({theme,setTheme}) => {
+const Main = ({ theme, setTheme }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [regionFilter, setRegionFilter] = useState("");
 
-  const [searchQuery,setSearchQuery] = useState('');
-  const [countries,setCountries] =  useState([]);
-  const [isLoading,setIsLoading] = useState(false);
-  const [error, setError] = useState('');
-  const [regionFilter,setRegionFilter] = useState('');
- 
   const allCountries = useRef([]);
-  
+
   const dropDownRef = useRef(null);
 
-
   // On Initial mount fetching all the countries data and caching it in allCountries using useRef hook.
-  useEffect(function(){
-
+  useEffect(function () {
     async function fetchData() {
-        try {
-            setIsLoading(true)
-            setError('');
-            const response = await fetch(`https://restcountries.com/v3.1/all`);
-            if(!response.ok){
-              throw new Error(`Http Error: ${response.status}`)
-            }
-            const jsonData = await response.json();
-            allCountries.current = jsonData
-            setCountries(allCountries.current);
-            setIsLoading(false)
-         
-        } 
-        catch(e){
-              setIsLoading(false)
-              
-              setError(e.message)
+      try {
+        setIsLoading(true);
+        setError("");
+        const response = await fetch(
+          `https://restcountries.com/v3.1/all?fields=name,capital,flags,population,region,subregion,tld,currencies,languages,borders`
+        );
+        if (!response.ok) {
+          throw new Error(`Http Error: ${response.status}`);
         }
+        const jsonData = await response.json();
+        allCountries.current = jsonData;
+        setCountries(allCountries.current);
+        setIsLoading(false);
+      } catch (e) {
+        setIsLoading(false);
+
+        setError(e.message);
+      }
     }
 
     fetchData();
-
-  },[])
-
+  }, []);
 
   //OnSearch for a country
-  function onInput(event){
+  function onInput(event) {
+    setSearchQuery(event.target.value);
 
+    if (event.target.value == "") {
+      setCountries(allCountries.current);
+      return;
+    }
 
-       setSearchQuery(event.target.value);
-
-       if(event.target.value == ""){
-         setCountries(allCountries.current);
-         return;
-       }
-       
-       const newCountryList = allCountries.current.filter((country)=>{
-                 if(country.name.common.toLowerCase().includes(event.target.value.toLowerCase().trim()) ){
-                   return country
-                 }
-       })
-       setCountries(newCountryList)
-
-       
+    const newCountryList = allCountries.current.filter((country) => {
+      if (
+        country.name.common
+          .toLowerCase()
+          .includes(event.target.value.toLowerCase().trim())
+      ) {
+        return country;
+      }
+    });
+    setCountries(newCountryList);
   }
 
-  function onRegionFilterChange(event){
-       setRegionFilter(event.target.innerText);
-       const newCountryList = allCountries.current.filter((country)=>{
-             
-               if(country.region.toLowerCase().includes(event.target.innerText.toLowerCase())){
-                 return country
-               }
-       })
-       setCountries(newCountryList);
-        toggleDropDownList();
+  function onRegionFilterChange(event) {
+    setRegionFilter(event.target.innerText);
+    const newCountryList = allCountries.current.filter((country) => {
+      if (
+        country.region
+          .toLowerCase()
+          .includes(event.target.innerText.toLowerCase())
+      ) {
+        return country;
+      }
+    });
+    setCountries(newCountryList);
+    toggleDropDownList();
   }
 
-  function toggleDropDownList(){ 
+  function toggleDropDownList() {
     const dropDown = dropDownRef.current.nextElementSibling;
-    dropDown.classList.toggle('hide-dropdown');
+    dropDown.classList.toggle("hide-dropdown");
   }
 
   return (
     <>
-    <Header theme={theme} setTheme={setTheme} />
-    <main className="flex flex-col gap-12 background-secondary w-screen h-screen  pt-12  px-4 lg:px-20 font-Nunito-sans overflow-y-auto text-primary">
-       <section className=' flex flex-col sm:flex-row gap-10  sm:h-[56px] sm:items-center sm:justify-between'>
-          <div className='flex flex-row items-center max-w-[480px] w-full  gap-6 rounded-sm background-primary  shadow-lg px-3.5 lg:px-5 py-4 lg:w-max-[200px]'>
-            <FontAwesomeIcon icon={faMagnifyingGlass}  className='text-primary'/>
-            <label > 
-                <input className={`${theme =='light' ? "text-Very-Dark-Blue" : "text-white" }`} type="text" name="country"  value={searchQuery} placeholder='Search for a country...' onChange={onInput}/>
+      <Header theme={theme} setTheme={setTheme} />
+      <main className="flex flex-col gap-12 background-secondary w-screen h-screen  pt-12  px-4 lg:px-20 font-Nunito-sans overflow-y-auto text-primary">
+        <section className=" flex flex-col sm:flex-row gap-10  sm:h-[56px] sm:items-center sm:justify-between">
+          <div className="flex flex-row items-center max-w-[480px] w-full  gap-6 rounded-sm background-primary  shadow-lg px-3.5 lg:px-5 py-4 lg:w-max-[200px]">
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              className="text-primary"
+            />
+            <label>
+              <input
+                className={`${
+                  theme == "light" ? "text-Very-Dark-Blue" : "text-white"
+                }`}
+                type="text"
+                name="country"
+                value={searchQuery}
+                placeholder="Search for a country..."
+                onChange={onInput}
+              />
             </label>
           </div>
-       {/*-- Custom Dropdown Structure */}
-       <CustomSelect regionFilters={regionFilters} onRegionFilterChange={onRegionFilterChange} dropDownRef={dropDownRef} toggleDropDownList={toggleDropDownList} theme={theme} regionFilter={regionFilter}/>
-
-       </section>
-       <section className='flex flex-row flex-wrap justify-center items-center gap-18'>
-           { isLoading && <div className="animate-spin w-5 h-5 border-t-2 border-b-2 border-primary rounded-full"></div>}
-           {
-             error && <p className='text-red-800'>{error}</p>
-           } 
-           { countries && countries.map((countryData,index)=>{
-               return <Country key={index} data={countryData}/>
-           })}
-       </section>
-    </main>
+          {/*-- Custom Dropdown Structure */}
+          <CustomSelect
+            regionFilters={regionFilters}
+            onRegionFilterChange={onRegionFilterChange}
+            dropDownRef={dropDownRef}
+            toggleDropDownList={toggleDropDownList}
+            theme={theme}
+            regionFilter={regionFilter}
+          />
+        </section>
+        <section className="flex flex-row flex-wrap justify-center items-center gap-18">
+          {isLoading && (
+            <div className="animate-spin w-5 h-5 border-t-2 border-b-2 border-primary rounded-full"></div>
+          )}
+          {error && <p className="text-red-800">{error}</p>}
+          {countries &&
+            countries.map((countryData, index) => {
+              return <Country key={index} data={countryData} />;
+            })}
+        </section>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
